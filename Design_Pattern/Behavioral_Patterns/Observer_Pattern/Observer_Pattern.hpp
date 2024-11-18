156acc5cbf0dcac10688d0bd6a26019e4f5c1e03
XNefertar <xnefertar@gmail.com>
2024-11-18 20:04:30 +0800

    Observer_Pattern_Done

--- Design_Pattern/Behavioral_Patterns/Observer_Pattern/Observer_Pattern.hpp
+++ Design_Pattern/Behavioral_Patterns/Observer_Pattern/Observer_Pattern.hpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define DEFAULT_TEMPERATURE 0.0
+#define DEFAULT_HUMIDITY    0.0
 
 class ObserverInterface
 {
 public:
     virtual void update(double temperature, double humidity) = 0;
-    virtual void display(double temperature, double humidity) = 0;
+    virtual void display() = 0;
 };
 
 class SubjectInterface
@@ -18,11 +23,65 @@ public:
 class WeatherData
 :public SubjectInterface
 {
+private:
+    std::vector<ObserverInterface*> _ObserverPtrArray;
+    double _Temperature;
+    double _Humidity;
+
 public:
+    WeatherData(double default_temperature = DEFAULT_TEMPERATURE, double default_humidity = DEFAULT_HUMIDITY)
+        : _Temperature(default_temperature),
+          _Humidity(default_humidity)
+    {}
+
     virtual void RegisterObserver(ObserverInterface* observer) override {
-
+        _ObserverPtrArray.push_back(observer);
     }
-    virtual void RemoveObserver(ObserverInterface *observer) override {
 
+    virtual void RemoveObserver(ObserverInterface *observer) override {
+        // Method 1
+        // std::find 找对应位置的 iterator
+        // auto it = std::find(_ObserverPtrArray.begin(), _ObserverPtrArray.end(), observer);
+        // if(it != _ObserverPtrArray.end()){
+        //     _ObserverPtrArray.erase(it);
+        // }
+
+        // Method 2
+        // std::remove 和 std::erase 结合
+        // std::move: 在给定的范围内找到对应的 observer, 并将其移至末尾
+        // std::erase: 使用重载, 在给定范围内删除所有元素
+        _ObserverPtrArray.erase(std::remove(_ObserverPtrArray.begin(), _ObserverPtrArray.end(), observer), _ObserverPtrArray.end());
+    }
+
+    void SetWeatherData(double default_temperature = DEFAULT_TEMPERATURE, double default_humidity = DEFAULT_HUMIDITY){
+        _Temperature = default_temperature;
+        _Humidity = default_humidity;
+    }
+
+    virtual void NotifyObserver(){
+        for(auto it : _ObserverPtrArray){
+            it->update(_Temperature, _Humidity);
+        }
+    }
+};
+
+class CurrentConditionDisplay
+:public ObserverInterface
+{
+private:
+    double _Temperature;
+    double _Humidity;
+
+public:
+
+    virtual void display() override {
+        std::cout << "Current Condition Display \t" << std::endl;
+        std::cout << "The current temperature is " << _Temperature << ", and the humidity is " << _Humidity << "." << std::endl;
+    }
+
+    virtual void update(double Temperature, double Humidity) override {
+        _Temperature = Temperature;
+        _Humidity = Humidity;
+        display();
     }
 };
